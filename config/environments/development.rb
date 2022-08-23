d06eeae9395093486c60bee7484046295b116a12
sauzerhokuto <108565543+sauzerhokuto@users.noreply.github.com>
2022-08-23 12:02:41 +0900

    Revert "Task6〜10までのデータ"

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts = ["6b70-27-141-50-144.jp.ngrok.io", "localhost"]
 end
